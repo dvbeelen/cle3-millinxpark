d389be93f046b4797ea26f503a1829895138502f
dvanbeelen <>
2019-03-11 15:15:57 +0100

    Wrote pseudo-code
    Wrote a basic description of the application we are going to make.

---
+++ Pseudocode/Pseudocode.ino
@@ -0,0 +1,28 @@
+/* START
+ *  Druksensor registreert persoon aanwezigheid
+ *  Arduino gaat aan
+ *  LEDs en geluid gaan aan om aan te geven dat het aanstaat
+ *  
+ *  API
+ *  - Toevoegen nummer halen uit API
+ *  - Kies een van de vijf nummers op telefoon
+ *  - QR codes
+ *  
+ *  WERKING
+ *  Aftellen - Licht
+ *  Eerste ronde - LED gaat aan
+ *  Gebruiker gaat naar druksensorplaat bij de blomepot
+ *  LED gaat aan in knipperen in een van de bloempotten, geeft aan dat de gebruiker daar straks heen moet
+ *  LED blijven 10 sec. aan
+ *  LED gaan weer knipperen
+ *  
+ *  EINDE
+ *  Als speler LEDS niet bereikt gaan ze uit en is het game over. 
+ *  Anders duurt het nummer een minuut lang. Speler probeert het nummer zo direct mogelijk te doen. 
+ *  
+
+ *   
+ * 
+ *  
+ *  
+ */
