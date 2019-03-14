aa78a3584cab9e2e53b904bae2b685873afab38f
dvanbeelen <>
2019-03-14 10:30:37 +0100

    Revert "Made minor changes to pseudocode"
    This reverts commit a038f06ca396d08791973777248cef5cd3d89a5c.

--- Pseudocode/Pseudocode.ino
+++ Pseudocode/Pseudocode.ino
@@ -1,5 +1,5 @@
 /* START
- *  Druksensor registreert persoon aanwezigheid / Knop wordt ingedrukt om te starten
+ *  Druksensor registreert persoon aanwezigheid
  *  Arduino gaat aan
  *  LEDs en geluid gaan aan om aan te geven dat het aanstaat
  *  
