31ce2c27a820835bb00b283a1e919a8091493fa2
dvanbeelen <>
2019-03-20 10:03:23 +0100

    Update demo_v1.ino

--- maquette_code/demo_v1/demo_v1.ino
+++ maquette_code/demo_v1/demo_v1.ino
@@ -1,9 +1,10 @@
-int led1 = 10;
+int led1 = 8;
 int led2 = 9;
-int led3 = 11;
-int led4 = 7;
-int led5 = 6;
+int led3 = 10;
+int led4 = 11;
+int led5 = 12;
 int button = 4;
+int buttonPressed = 0;
 
 /*Leds assigned to pins */
 void setup()
@@ -19,24 +20,31 @@ void setup()
 
 /*Leds blink one by one */
 void loop(){
- digitalWrite(led1,HIGH);
-  delay(500);
- digitalWrite(led1,LOW);
-  delay(500);
- digitalWrite(led2,HIGH);
-  delay(500);
- digitalWrite(led2,LOW);
-  delay(500);
- 
-  delay(500);
- digitalWrite(led4,HIGH);
-  delay(500);
- digitalWrite(led4,LOW);
-  delay(500);
- digitalWrite(led5,HIGH);
-  delay(500);
- digitalWrite(led5,LOW);
-  delay(500);
+  if(buttonPressed == 1) { // button has never been pressed
+    // detect button press
+   int buttonState = digitalRead(button);
+  } else { // button has been pressed, do this instead
+  digitalWrite(led1,HIGH);
+    delay(500);
+  digitalWrite(led1,LOW);
+    delay(500);
+  digitalWrite(led2,HIGH);
+    delay(500);
+  digitalWrite(led2,LOW);
+    delay(500);
+  digitalWrite(led2,HIGH);
+    delay(500);
+  digitalWrite(led2,LOW);
+    delay(500);
+  digitalWrite(led4,HIGH);
+    delay(500);
+  digitalWrite(led4,LOW);
+    delay(500);
+  digitalWrite(led5,HIGH);
+    delay(500);
+  digitalWrite(led5,LOW);
+    delay(500);
+  }
 }
 
 /*
