69313ddff597b6199cda9651017c2029c97946a9
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2019-04-03 15:18:37 +0200

    Establish API Connection
    - Connects Unsplash API to the application

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -1,7 +1,17 @@
+let gameScore = 690;
+
+//API connection
+import Unsplash, { toJson } from "unsplash-js";
+
+const unsplash = new Unsplash({
+  applicationId: "3fea5ddfe37456e9398935c41ba6ec2d556d70aea53af1ddc80279ae9c1f8a1f",
+  secret: "3477463452e420e5b643184c1c2cdd2835464b892ab7bb2eccb9cd4754a1e7e4"
+});
+
 
 //Slider navigation
 //This function brings the user on click from the startscreen to the screen where they can pick a song.
-function pickSong() {
+function nextToSongs() {
     const currentDiv = document.getElementById('slide-4');
     const nextDiv = document.getElementById('songSelect');
     const clone = nextDiv.cloneNode(true);
@@ -12,7 +22,7 @@ function pickSong() {
 }
 
 //This function brings the user on click from the screen where they can pick a song to the gamescreen.
- function gameStart() {
+ function nextToGame() {
         const currentDiv = document.getElementById('songSelect');
         const nextDiv = document.getElementById('game');
         const clone = nextDiv.cloneNode(true);
@@ -20,7 +30,6 @@ function pickSong() {
         while (currentDiv.firstChild) currentDiv.firstChild.remove();
     
         currentDiv.appendChild(clone);
+
+        let score = document.getElementById("score").innerHTML = gameScore;
     }
-
-
-//Game
