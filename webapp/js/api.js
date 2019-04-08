d0280ce54a2a952ed4b6038506c6255faf225c09
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2019-04-08 11:47:28 +0200

    Update api.js

--- webapp/js/api.js
+++ webapp/js/api.js
@@ -1,18 +1,21 @@
 const imageWidth = 200; //your desired image width in pixels
-const imageHeight = 200; //desired image height in pixels
+const imageHeight = 200; //desired image height in pixel
 const collectionID = 4576130; //the collection ID from the original url
+const images = document.getElementsByClassName("pickGenre");
 const genres = ["music,classical", "music,rock", "music,hiphop", "music,videogames"]; //Search terms 
 
 //Deze code zou voor ieder array-item een afbeelding op moeten halen uit de API en dan vervolgens de src van de pickGenre afbeeldingen
-// aanpassen, maar ik krijg of 4x dezelfde image of helemaal niets, wat doe ik fout?
+//moeten aanpassen, maar ik krijg of 4x dezelfde image of helemaal niets, wat doe ik fout?
+
 for (i=0; i<genres.length; i++){
 fetch(`https://source.unsplash.com/${imageWidth}x${imageHeight}/?${genres[i]}`)
  .then(data => changeSongPic(data))
- function changeSongPic(data){
-       document.getElementsByClassName("pickGenre"[i]) = data.url;
+function changeSongPic(data){
+       images.src = data.url;
        console.log(data);
   }
  }
+
 
 // fetch(`https://source.unsplash.com/${imageWidth}x${imageHeight}/?${genres[0]}`)
 //     .then(data => changeSongPic(data))
