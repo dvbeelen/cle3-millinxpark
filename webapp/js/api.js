2ecc7616f6f752b9e6e45e0a9a9620e0ff350526
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2019-04-08 11:36:59 +0200

    Added comments for api.js
    - These comments are meant to help my teammates understand what the code is supossed to do

--- webapp/js/api.js
+++ webapp/js/api.js
@@ -3,36 +3,40 @@ const imageHeight = 200; //desired image height in pixels
 const collectionID = 4576130; //the collection ID from the original url
 const genres = ["music,classical", "music,rock", "music,hiphop", "music,videogames"]; //Search terms 
 
-fetch(`https://source.unsplash.com/${imageWidth}x${imageHeight}/?${genres[0]}`)
-    .then(data => changeSongPic(data))
-        function changeSongPic(data){
-        document.getElementsByClassName("pickGenre"[0]).src = data.url;
-}
+//Deze code zou voor ieder array-item een afbeelding op moeten halen uit de API en dan vervolgens de src van de pickGenre afbeeldingen
+// aanpassen, maar ik krijg of 4x dezelfde image of helemaal niets, wat doe ik fout?
+for (i=0; i<genres.length; i++){
+fetch(`https://source.unsplash.com/${imageWidth}x${imageHeight}/?${genres[i]}`)
+ .then(data => changeSongPic(data))
+ function changeSongPic(data){
+       document.getElementsByClassName("pickGenre"[i]) = data.url;
+       console.log(data);
+  }
+ }
 
-fetch(`https://source.unsplash.com/${imageWidth}x${imageHeight}/?${genres[1]}`)
-    .then(data => changeSongPic(data))
-        function changeSongPic(data){
-        document.getElementsByClassName("pickGenre"[1]).src = data.url;
-}
+// fetch(`https://source.unsplash.com/${imageWidth}x${imageHeight}/?${genres[0]}`)
+//     .then(data => changeSongPic(data))
+//         function changeSongPic(data){
+//         document.getElementsByClassName("pickGenre"[0]).src = data.url;
+// }
 
-fetch(`https://source.unsplash.com/${imageWidth}x${imageHeight}/?${genres[2]}`)
-    .then(data => changeSongPic(data))
-        function changeSongPic(data){
-        document.getElementsByClassName("pickGenre"[2]).src = data.url;
-}
+// fetch(`https://source.unsplash.com/${imageWidth}x${imageHeight}/?${genres[1]}`)
+//     .then(data => changeSongPic(data))
+//         function changeSongPic(data){
+//         document.getElementsByClassName("pickGenre"[1]).src = data.url;
+// }
 
-fetch(`https://source.unsplash.com/${imageWidth}x${imageHeight}/?${genres[3]}`)
-    .then(data => changeSongPic(data))
-        function changeSongPic(data){
-        document.getElementsByClassName("pickGenre"[3]).src = data.url;
-}
-//  .then(data => changeSongPic(data))
-// for (i=0; i<genres.length; i++){
-// fetch(`https://source.unsplash.com/${imageWidth}x${imageHeight}/?${genres[i]}`)
-//  .then(data => changeSongPic(data))
-//  function changeSongPic(data){
-//        document.getElementsByClassName("pickGenre"[i]) = data.url;
-//        console.log(data);
-//   }
-//  }
+// fetch(`https://source.unsplash.com/${imageWidth}x${imageHeight}/?${genres[2]}`)
+//     .then(data => changeSongPic(data))
+//         function changeSongPic(data){
+//         document.getElementsByClassName("pickGenre"[2]).src = data.url;
+// }
+
+// fetch(`https://source.unsplash.com/${imageWidth}x${imageHeight}/?${genres[3]}`)
+//     .then(data => changeSongPic(data))
+//         function changeSongPic(data){
+//         document.getElementsByClassName("pickGenre"[3]).src = data.url;
+// }
+
+
  
