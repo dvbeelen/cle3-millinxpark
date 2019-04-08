953519fb055b78a09c069a93f2ba87572d036f3d
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2019-04-08 16:33:08 +0200

    Add function that selects a genre
    -Adds function that saves the picture of the genre you choose and displays it in the gamescreen and resultscreen

--- webapp/js/main.js
+++ webapp/js/main.js
@@ -1,5 +1,8 @@
 let gameScore = 0;
-let gameEnd = document.getElementById("endGame")
+let gameEnd = document.getElementById("endGame");
+let gameSong = document.getElementsByClassName("gameSong");
+let pickGenre = document.getElementsByClassName("pickGenre");
+let p = 0;
 
 //Adds 150 points to players score every 5 seconds
 setInterval(function upScore() {
@@ -12,3 +15,24 @@ gameEnd.addEventListener("click", function gameEnd(){
     gameScore = gameScore
     document.getElementById("endScore").innerHTML = gameScore
 })
+
+function saveChoice(){
+  if (p == 1){
+    gameSong[0].src = pickGenre[0].src;
+    gameSong[1].src = pickGenre[0].src;
+    console.log(p);
+  } else if (p == 2){
+    gameSong[0].src = pickGenre[1].src;
+    gameSong[1].src = pickGenre[1].src;
+    console.log(p);
+  } else if (p == 3){
+    gameSong[0].src = pickGenre[2].src;
+    gameSong[1].src = pickGenre[2].src;
+    console.log(p);
+  } else if (p == 4){
+    gameSong[0].src = pickGenre[3].src;
+    gameSong[1].src = pickGenre[3].src;
+    console.log(p);
+  }
+}
+
