43e103ff97b2c890ecc53aed754572ec6dd51407
dvanbeelen <34212264+dvbeelen@users.noreply.github.com>
2019-04-08 11:31:51 +0200

    Attempt to change pictures with API
    - This is NOT the working API version. This is a test-version to be reviewed by teammembers

--- webapp/js/api.js
+++ webapp/js/api.js
@@ -1,11 +1,38 @@
 const imageWidth = 200; //your desired image width in pixels
 const imageHeight = 200; //desired image height in pixels
 const collectionID = 4576130; //the collection ID from the original url
-const image = document.getElementsByClassName("pickGenre");
+const genres = ["music,classical", "music,rock", "music,hiphop", "music,videogames"]; //Search terms 
 
-fetch(`https://source.unsplash.com/collection/${collectionID}/${imageWidth}x${imageHeight}`)
- .then(data => changeSongPic(data)) 
- function changeSongPic(data){
-  console.log(data.url)
- }
+fetch(`https://source.unsplash.com/${imageWidth}x${imageHeight}/?${genres[0]}`)
+    .then(data => changeSongPic(data))
+        function changeSongPic(data){
+        document.getElementsByClassName("pickGenre"[0]).src = data.url;
+}
 
+fetch(`https://source.unsplash.com/${imageWidth}x${imageHeight}/?${genres[1]}`)
+    .then(data => changeSongPic(data))
+        function changeSongPic(data){
+        document.getElementsByClassName("pickGenre"[1]).src = data.url;
+}
+
+fetch(`https://source.unsplash.com/${imageWidth}x${imageHeight}/?${genres[2]}`)
+    .then(data => changeSongPic(data))
+        function changeSongPic(data){
+        document.getElementsByClassName("pickGenre"[2]).src = data.url;
+}
+
+fetch(`https://source.unsplash.com/${imageWidth}x${imageHeight}/?${genres[3]}`)
+    .then(data => changeSongPic(data))
+        function changeSongPic(data){
+        document.getElementsByClassName("pickGenre"[3]).src = data.url;
+}
+//  .then(data => changeSongPic(data))
+// for (i=0; i<genres.length; i++){
+// fetch(`https://source.unsplash.com/${imageWidth}x${imageHeight}/?${genres[i]}`)
+//  .then(data => changeSongPic(data))
+//  function changeSongPic(data){
+//        document.getElementsByClassName("pickGenre"[i]) = data.url;
+//        console.log(data);
+//   }
+//  }
+ 
--- webapp/js/main.js
+++ webapp/js/main.js
@@ -1,31 +1,14 @@
 let gameScore = 0;
+let gameEnd = document.getElementById("endGame")
 
-//Slider navigation
-//This function brings the user on click from the startscreen to the screen where they can pick a song.
-function nextToSongs() {
-    const currentDiv = document.getElementById('slide-4');
-    const nextDiv = document.getElementById('songSelect');
-    const clone = nextDiv.cloneNode(true);
-
-    while (currentDiv.firstChild) currentDiv.firstChild.remove();
-
-    currentDiv.appendChild(clone);
-}
-
-//This function brings the user on click from the screen where they can pick a song to the gamescreen.
- function nextToGame() {
-        const currentDiv = document.getElementById('songSelect');
-        const nextDiv = document.getElementById('game');
-        const clone = nextDiv.cloneNode(true);
-    
-        while (currentDiv.firstChild) currentDiv.firstChild.remove();
-    
-        currentDiv.appendChild(clone);
-        upScore();
-    }
-
+//Adds 150 points to players score every 5 seconds
 setInterval(function upScore() {
     gameScore = gameScore + 150;
     document.getElementById("score").innerHTML = gameScore;
   }, 5000);
 
+//Stops the adding of score, gives player endresult
+gameEnd.addEventListener("click", function gameEnd(){
+    gameScore = gameScore
+    document.getElementById("endScore").innerHTML = gameScore
+})
